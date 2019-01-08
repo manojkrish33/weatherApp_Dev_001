05cfed8299b0e31a8926ad20512f5aef09de4f68
Manoj  Muktevi <971748@TCS.com>
2019-01-08 17:25:56 +0530

    Integrating Input box to App

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
+import InputBox from './inputBox';
 
 class App extends Component {
+
+  state = {
+    lat : "",
+    long : "",
+    showOutput : false,
+    errorInput :false
+  }
+
+  latitudeChange = (event) => {
+    const re = /^[-]?[0-9]*\.?[0-9]*/;
+    let updatedLatitude = event.target.value;
+    if(event.target.value ==='' || re.test(event.target.value)){
+      this.setState(
+        {
+          lat : updatedLatitude
+        }
+      )
+    }  
+  }
+
+  longitudeChange = (event) => {
+    const re = /^[-]?[0-9]*\.?[0-9]*/;
+    let updatedLongitude = event.target.value;
+    if(event.target.value ==='' || re.test(event.target.value)){
+      this.setState(
+        {
+          long : updatedLongitude
+        }
+      )
+    }
+  }
+
   render() {
     return (
       <div className="App-header">
-        <header className="Allign-center row"  >
+        <header className="Allign-center row">
           <h3 className="col-md-12"> Weather Application </h3>
           <div className="col-md-2"></div>
           <p className="col-md-8">Enter Latitude and Longitude to get the weather updates</p>
           <div className="col-md-2"></div>
         </header>
+        <div className="row margin-top-30px">
+          <InputBox name='Latitude' onChanged={this.latitudeChange} value={this.state.lat}/>
+          <InputBox name='Longitude' onChanged={this.longitudeChange} value={this.state.long}/>
+        </div>
       </div>
     );
   }
--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../Components/App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import InputBox from '../Components/inputBox';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('<App />' , () => {
+
+  let wrapper;
+  beforeEach(() => {wrapper = shallow(<App />);});
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('checks if input boxes are avaialbale to enter data' , () => {
+    expect(wrapper.find(InputBox)).toHaveLength(2);
+  });
+
+  it('chcek if latitude change function is working as expected', () => {
+    wrapper.instance().latitudeChange({target : { value : "14"}});
+    expect(wrapper.state().lat).toEqual("14");
+  });
+
+  it('chcek if longitude change function is working as expected', () => {
+    wrapper.instance().longitudeChange({target : { value : "132"}});
+    expect(wrapper.state().long).toEqual("132");
+  });
+  
 });
